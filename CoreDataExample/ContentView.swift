8e3392186847224f0e5a428ecf631a90abf9f62d
cihangirtuncer <cihangirtuncer184@gmail.com>
2022-11-08 16:24:17 +0300

    [feature] core data and mvvm example completed

--- CoreDataExample/ContentView.swift
+++ CoreDataExample/ContentView.swift
@@ -1,6 +1,6 @@
 //
-//  ContentView.swift
-//  CoreDataExample
+//  CoreDataBootcamp.swift
+//  SwiftUIBootCamp
 //
 //  Created by Cihangir Tuncer on 6.11.2022.
 //
@@ -8,109 +8,137 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
 
-    @FetchRequest(
-        entity: FruitEntity.entity(),
-        sortDescriptors: [NSSortDescriptor(keyPath: \FruitEntity.name, ascending: true)])
-    var fruits : FetchedResults<FruitEntity>
+class CoreDataViewModel: ObservableObject {
+    
+    let container : NSPersistentContainer
+    @Published var savedEntities : [FruitEntity] = []
+    
+    init() {
+        container = NSPersistentContainer(name: "CoreDataExample")
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("ERROR LOADING CORE DATA. \(error)")
+            }
+        }
+        fetchFruits()
+    }
+   func fetchFruits(){
+        let request = NSFetchRequest<FruitEntity>(entityName: "FruitEntity")
+       do{
+          savedEntities =  try container.viewContext.fetch(request)
+       }catch let error{
+           print("Error fetching..\(error)")
+       }
+    }
+ 
+    func addFruit(text : String) {
+        let newFruit = FruitEntity(context: container.viewContext)
+        newFruit.name = text
+        saveData()
+    }
+    
+    func deleteFruit(indexSet: IndexSet) {
+        guard let index = indexSet.first else {return}
+        let entity = savedEntities[index]
+        container.viewContext.delete(entity)
+        saveData()
+    }
+    
+    func updateFruit(entity : FruitEntity, newNameFruit : String){
+        entity.name = newNameFruit
+        saveData()
+    }
+    
+    func saveData() {
+        do{
+            try container.viewContext.save()
+            fetchFruits()
+        }catch let error{
+            print("Error saving... \(error)")
+        }
+    }
+    
+}
+
+
+
+struct CoreDataExample: View {
+    @StateObject var vm = CoreDataViewModel()
     @State var textFieldText : String = ""
+    @State var isPresent : Bool = false
+    @State var textFieldTextAlertUpdate : String = ""
+
     var body: some View {
-        NavigationView {
-            VStack(spacing:20) {
-                
-                TextField("Add fruits here...",text: $textFieldText)
+        NavigationStack{
+            VStack {
+                TextField("Add fruit here...", text: $textFieldText)
                     .font(.headline)
                     .padding(.leading)
-                    .frame(maxWidth: .infinity)
                     .frame(height: 55)
                     .background(Color.gray.opacity(0.2))
                     .cornerRadius(10)
                     .padding(.horizontal)
                 
                 Button {
-                    addItem()
+                    guard !textFieldText.isEmpty else {return }
+                    vm.addFruit(text: textFieldText)
+                    textFieldText = ""
                 } label: {
                     Text("Button")
                         .font(.headline)
-                        .foregroundColor(Color.black)
-                        .frame(maxWidth: .infinity)
+                        .foregroundColor(Color.white)
                         .frame(height: 55)
-                        .background(Color.white)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.cyan)
                         .cornerRadius(10)
-                        .shadow(radius: 10)
-                        .padding()
                 }
                 .padding(.horizontal)
-
+                    
                 List {
-                    ForEach(fruits) { fruit in
-                            Text(fruit.name ?? "")
+                    ForEach(vm.savedEntities) { entity in
+                        Text(entity.name ?? "NO NAME")
                             .onTapGesture {
-                                updateItem(fruit: fruit)
+                                isPresent.toggle()
                             }
-                        
+                            .alert("Please add a fruit name", isPresented: $isPresent, actions: {
+                                VStack {
+                                    TextField("Write here..", text:$textFieldTextAlertUpdate)
+                                    
+                                }
+                                Button {
+                                    if textFieldTextAlertUpdate != ""{
+                                        vm.updateFruit(entity: entity, newNameFruit: textFieldTextAlertUpdate )
+                                        textFieldTextAlertUpdate = ""
+                                    }
+                                    
+                                } label: {
+                                    Text("Save")
+                                                                    }
+                                Button {
+                                    
+                                } label: {
+                                    Text("Cancel")
+                                }
+
+                                
+
+
+
+                                                            })
                     }
-                    .onDelete(perform: deleteItems)
+                    .onDelete(perform: vm.deleteFruit)
                 }
+                .listStyle(PlainListStyle())
             }
-            .listStyle(PlainListStyle())
             .navigationTitle("Fruits")
-            .navigationBarItems(leading: EditButton())
-            Text("Select an item")
         }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newFruit = FruitEntity(context: viewContext)
-            newFruit.name = textFieldText
-            saveItems()
-            textFieldText = ""
-        }
-    }
-    
-    private func updateItem(fruit : FruitEntity){
-        withAnimation {
-            let currentName = fruit.name ?? ""
-            let newName = currentName + "!"
-            fruit.name = newName
-            saveItems()
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-        //    offsets.map { items[$0] }.forEach(viewContext.delete)
-            guard let index = offsets.first else {return}
-            let fruitEntity = fruits[index]
-            viewContext.delete(fruitEntity)
-            saveItems()
-        }
-    }
-    
-    private func saveItems(){
-        do {
-            try viewContext.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
+        
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
-struct ContentView_Previews: PreviewProvider {
+struct CoreDataBootcamp_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        CoreDataExample()
     }
 }
